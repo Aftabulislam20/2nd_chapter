8f94624b74db56bac28e421b1fb5cfbc72b78529
Aftabul Islam <raficomilla18@gmail.com>
2025-10-22 12:05:40 +0600

    Updated

---
+++ Chapter_2.py
@@ -0,0 +1,40 @@
+# Basics of python
+'''
+First release   = 20 February 1991
+paradyme        = Multi peradyme
+Creator         = Guido van Rosum
+Developer       = Python Softwear Foundation
+Digital Publish = 3.14.0 / 7 October 2025
+'''
+#Features Of Python
+'''
+1. Easy to Code
+2. Free and open source
+3. Object-oriented language
+4. GUI programming support
+5. High level language
+6. Extensible Feature
+7. Python is portable language
+8. python is integrated language
+9. Interpreted language
+10. Large standard Library
+11. Dynamically tped language
+'''
+#Uses of python
+'''
+Web softwear creation
+Automation Softwear Creation
+Bio informatics
+Machine learning
+Natural language processing
+Web crular create
+Imunity security tools
+Graphical user interface create
+web application security scanner
+Database Application
+Cyber security
+Distributed Programming
+Internet Scripting
+Data analysis
+Core security tools
+'''
